237109ce06601052d017f535bb636c5ae5d776ad
chi28092 <sarmaji.m@gmail.com>
2022-03-12 17:19:08 +0530

    MVC app created by kamesh
    MVC app created by kamesh

---
+++ mvcapp/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using mvcapp.Models;
+
+namespace mvcapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly string _filePath;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _filePath = configuration["FileConfiguration:Filepath"];
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult Index(PersonModel person)
+        {
+
+            PushPersonDetailsToFile(person);
+            return View();
+        }
+
+        private void PushPersonDetailsToFile(PersonModel person)
+        {
+            var path = string.Format(_filePath, $"{DateTime.Today:MM-dd-yy}-{DateTime.Now:%H}");
+            byte[] utf8bytesJson = JsonSerializer.SerializeToUtf8Bytes(person);
+            string strResult = System.Text.Encoding.UTF8.GetString(utf8bytesJson);
+
+
+            // using (StreamWriter sw = System.IO.File.AppendText(path))
+            using (StreamWriter sw = (System.IO.File.Exists(path)) ? System.IO.File.AppendText(path) : System.IO.File.CreateText(path))
+
+            {
+                sw.WriteLine(strResult);
+
+            }
+
+        }
+    }
+}
---
+++ mvcapp/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcapp.Models
+{
+    public class PersonModel
+    {
+        ///<summary>
+        /// Gets or sets FirstName.
+        ///</summary>
+        public string FirstName { get; set; }
+
+        ///<summary>
+        /// Gets or sets LastName.
+        ///</summary>
+        public string LastName { get; set; }
+
+    }
+}
